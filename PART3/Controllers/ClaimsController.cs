5fedb69f6412058deec054f32691428572be6d0a
Lesedi Molutsi <ST10291938@rcconnect.edu.za>
2024-11-12 12:49:38 +0000

    Added new controller

---
+++ PART3/Controllers/ClaimsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PART3.Data;
+using PART3.Models;
+
+namespace PART3.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClaimsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Claims
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Lecturers.ToListAsync());
+        }
+
+        // GET: Claims/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecturer);
+        }
+
+        // GET: Claims/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Claims/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,lecture_ID,lecturer_Name,lecturer_Surname,lecturer_Email,lecturer_Contact,Program,Module_Code,Hours_Worked,Hourly_Rate,Date_Of_Session,UploadedFileName,Status")] Lecturer lecturer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lecturer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecturer);
+        }
+
+        // GET: Claims/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers.FindAsync(id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+            return View(lecturer);
+        }
+
+        // POST: Claims/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,lecture_ID,lecturer_Name,lecturer_Surname,lecturer_Email,lecturer_Contact,Program,Module_Code,Hours_Worked,Hourly_Rate,Date_Of_Session,UploadedFileName,Status")] Lecturer lecturer)
+        {
+            if (id != lecturer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lecturer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LecturerExists(lecturer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lecturer);
+        }
+
+        // GET: Claims/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecturer);
+        }
+
+        // POST: Claims/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lecturer = await _context.Lecturers.FindAsync(id);
+            if (lecturer != null)
+            {
+                _context.Lecturers.Remove(lecturer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LecturerExists(int id)
+        {
+            return _context.Lecturers.Any(e => e.Id == id);
+        }
+    }
+}
